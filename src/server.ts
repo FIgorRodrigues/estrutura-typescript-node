978f8b5acc0854f464b935e76c671005ddb39779
Igor <igor.oliveira@cocobambu.com>
2021-08-14 20:40:57 -0300

    eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World!" });
-});
+app.use(express.json());
+
+app.get('/', (request, response) => response.json({ message: 'Hello World!' }));
 
 app.listen(3333);
